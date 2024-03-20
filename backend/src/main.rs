bd99eb14377d5b887d2eb2583ba75872ba1f9e5d
RustyMaster <dawidmajka@protonmail.com>
2024-03-20 18:18:34 +0100

    Tracing and errors

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,12 +6,15 @@ use backend_server::{GetValueRequest, GetValueResponse, InsertValueRequest, Inse
 
 use tokio::sync::Mutex;
 use tonic::{transport::Server, Request, Response, Status};
+use tracing::{error, info};
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
+use uuid::Uuid;
 
 pub mod backend_server {
     tonic::include_proto!("kv");
 }
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 pub struct BackendService {
     database: Arc<Mutex<HashMap<String, String>>>,
 }
@@ -26,35 +29,40 @@ impl BackendService {
 
 #[tonic::async_trait]
 impl Kv for BackendService {
+    #[tracing::instrument(skip(self))]
     async fn insert_value(
         &self,
         request: Request<InsertValueRequest>,
     ) -> Result<Response<InsertValueResponse>, Status> {
-        println!("Got insert request: {:?}", request);
-
         let request = request.into_inner();
 
         let mut database = self.database.lock().await;
 
+        info!("Inserting data to database.");
+
         database.insert(request.key, request.value);
+
+        info!("Data inserted succesfully");
 
         let reply = InsertValueResponse { success: true };
 
         Ok(Response::new(reply))
     }
 
+    #[tracing::instrument(skip(self))]
     async fn get_value(
         &self,
         request: Request<GetValueRequest>,
     ) -> Result<Response<GetValueResponse>, Status> {
-        println!("Got get request: {:?}", request);
-
         let request = request.into_inner();
 
         let database = self.database.lock().await;
 
+        info!("Retrieving data from database.");
+
         match database.get(&request.key) {
             Some(value) => {
+                info!("Value from db: {:?}", value);
                 let reply = GetValueResponse {
                     value: value.to_string(),
                 };
@@ -62,6 +70,7 @@ impl Kv for BackendService {
                 return Ok(Response::new(reply));
             }
             None => {
+                error!("Value for key: {} not found.", &request.key);
                 let status =
                     Status::not_found(format!("Value for key: {} not found.", &request.key));
 
@@ -73,12 +82,21 @@ impl Kv for BackendService {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
+    let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    FmtSubscriber::builder().with_env_filter(filter).init();
+
+    let address = "[::1]:50051".parse()?;
     let backend_service = BackendService::new();
 
+    tracing::info!(message = "Starting server.", %address);
+
     Server::builder()
+        .trace_fn(|_| {
+            let request_id = Uuid::new_v4();
+            tracing::info_span!("Request span", %request_id)
+        })
         .add_service(KvServer::new(backend_service))
-        .serve(addr)
+        .serve(address)
         .await?;
 
     Ok(())
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,11 +1,14 @@
-use std::sync::Arc;
+use std::time::Duration;
 
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
 
 use backend_server::kv_client::KvClient;
-use tokio::sync::Mutex;
-use tonic::transport::Channel;
+use tokio::time::sleep;
+use tonic::{transport::Channel, Code};
+use tracing::{error, info, warn};
+use tracing_actix_web::TracingLogger;
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
 
 use crate::backend_server::{GetValueRequest, InsertValueRequest};
 
@@ -19,20 +22,23 @@ struct KV {
     value: String,
 }
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().body("Hello")
-}
-
+#[tracing::instrument(
+    skip(path, kv_client)
+    fields(
+        key = %path.as_str()
+    )
+)]
 async fn get_value(
     path: web::Path<String>,
     kv_client: web::Data<KvClient<Channel>>,
 ) -> impl Responder {
     let key = path.into_inner();
-    println!("Got GET request with key: {}", key);
 
     let mut kv_client = kv_client.get_ref().clone();
 
     let request = GetValueRequest { key };
+
+    info!("Sending request to grpc server: {:?}", &request);
 
     let response = kv_client.get_value(request).await;
 
@@ -40,52 +46,86 @@ async fn get_value(
         Ok(response) => {
             let value = response.into_inner().value;
 
-            println!("Value returned from backend server: {}", &value);
+            info!("Value returned from backend server: {}", &value);
 
             HttpResponse::Ok().body(value)
         }
         Err(status) => {
-            println!("Error returned from backend server: {:?}", &status);
-            HttpResponse::NotFound().finish()
+            error!("Error returned from backend server: {:?}", &status);
+
+            if status.code() == Code::NotFound {
+                HttpResponse::NotFound().finish()
+            } else {
+                HttpResponse::InternalServerError().finish()
+            }
         }
     }
 }
 
+#[tracing::instrument(skip(kv_client))]
 async fn insert_value(
     json_data: web::Json<KV>,
     kv_client: web::Data<KvClient<Channel>>,
 ) -> impl Responder {
-    println!("Got POST request with KV: {:?}", json_data);
-
     let mut kv_client = kv_client.get_ref().clone();
 
     let KV { key, value } = json_data.into_inner();
 
+    if key.trim().is_empty() {
+        warn!("Validation failed: key is empty.");
+        return HttpResponse::BadRequest().body("'key' field can't be empty.");
+    }
+
+    if value.trim().is_empty() {
+        warn!("Validation failed: value is empty.");
+        return HttpResponse::BadRequest().body("'value' field can't be empty.");
+    }
+
     let request = InsertValueRequest { key, value };
 
-    let response = kv_client
-        .insert_value(request)
-        .await
-        .expect("Should send insert request to backend server");
+    info!("Sending request to grpc server: {:?}", &request);
+    let response = kv_client.insert_value(request).await;
 
-    let value = response.into_inner().success;
+    match response {
+        Ok(response) => {
+            let value = response.into_inner().success;
+            info!("Value returned from backend server: {}", &value);
 
-    println!("Message returned from backend server: {}", &value);
-
-    HttpResponse::Ok().body("Success")
+            HttpResponse::Ok().finish()
+        }
+        Err(status) => {
+            error!("Error returned from backend server: {:?}", &status);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let kv_client = KvClient::connect("http://[::1]:50051")
-        .await
-        .expect("Connection with Backend service should be estabilished.");
+    let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    FmtSubscriber::builder().with_env_filter(filter).init();
+
+    let address = "http://[::1]:50051";
+
+    info!("Connecting to grpc server with address: {}", address);
+
+    let kv_client = match try_connect(address).await {
+        Ok(client) => {
+            info!("Connection estabilished.");
+            client
+        }
+        Err(e) => {
+            error!("Failed to connect to grpc server. Error: {:?}", e);
+            error!("Shutting down.");
+            std::process::exit(1);
+        }
+    };
 
     let kv_client = web::Data::new(kv_client);
 
     HttpServer::new(move || {
         App::new()
-            .route("/health_check", web::get().to(health_check))
+            .wrap(TracingLogger::default())
             .route("/{key}", web::get().to(get_value))
             .route("/", web::post().to(insert_value))
             .app_data(kv_client.clone())
@@ -93,4 +133,24 @@ async fn main() -> std::io::Result<()> {
     .bind("127.0.0.1:8000")?
     .run()
     .await
+}
+
+async fn try_connect(url: &str) -> Result<KvClient<Channel>, tonic::transport::Error> {
+    let mut attempt = 0;
+    let max_attempts = 5;
+    let base_delay = 500;
+
+    loop {
+        match KvClient::connect(url.to_owned()).await {
+            Ok(client) => return Ok(client),
+            Err(_) if attempt < max_attempts => {
+                let delay = Duration::from_millis(base_delay * 2_u64.pow(attempt));
+                warn!("Failed to connect to backend. Retry in {:?}...", delay);
+
+                sleep(delay).await;
+                attempt += 1;
+            }
+            Err(e) => return Err(e),
+        }
+    }
 }
